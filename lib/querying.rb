07b8761a13f01d0313dfee08fa35b3fb69efc13f
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-18 01:29:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,7 +40,7 @@ def select_name_and_series_subgenres_of_authors
   FROM authors, subgenres
   INNER JOIN series
   ON series.author_id = authors.id
-  WHERE series.author_id = authors.id
+  
   "
   #INNER JOIN subgenres
   #ON subgenres.series_id = series.id
